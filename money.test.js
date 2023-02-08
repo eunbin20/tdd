879313ebf78a4ba1b5a03700c003bfa2148bee88
eunbin20 <eunbines2@gmail.com>
2023-02-09 01:32:10 +0900

    feature: 2장. 타락한 객체

--- money.test.js
+++ money.test.js
@@ -3,6 +3,13 @@ import Dollar from "./money";
 test("test multiplication", () => {
   const five = new Dollar(5);
   five.times(2);
-
   expect(five.amount).toBe(10);
+});
+
+test("test multiplication, but value of Dollar should not be changed.", () => {
+  const five = new Dollar(5);
+  five.times(2);
+  expect(five.amount).toBe(10);
+  five.times(3);
+  expect(five.amount).toBe(15);
 });
