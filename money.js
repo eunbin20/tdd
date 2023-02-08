4ce9c306be46cfd2e99a307c651793df56ad4e44
eunbin20 <eunbines2@gmail.com>
2023-02-09 02:02:49 +0900

    feature: 3장. 모두를 위한 평등

--- money.js
+++ money.js
@@ -1,10 +1,14 @@
 /**
  * Todo List
- * -[] $5 + 10CHF = $10(환율이 2:1일 경우)
- * -[x] $5 * 2 = $10
- * -[] amount를 private으로 만들기
- * -[x] Dollar 부작용?
- * -[] Money 반올림?
+ * - [] $5 + 10CHF = $10(환율이 2:1일 경우)
+ * - [x] $5 * 2 = $10
+ * - [] amount를 private으로 만들기
+ * - [x] Dollar 부작용?
+ * - [] Money 반올림?
+ * - [x] equals()
+ * - [] hashCode()
+ * - [] Equal null
+ * - [] Equal object
  */
 
 export default class Dollar {
@@ -13,6 +17,10 @@ export default class Dollar {
   }
 
   times(multiplier) {
-    this.amount *= multiplier;
+    return new Dollar(this.amount * multiplier);
+  }
+
+  equals(object) {
+    return this.amount === object.amount;
   }
 }
--- money.test.js
+++ money.test.js
@@ -2,14 +2,19 @@ import Dollar from "./money";
 
 test("test multiplication", () => {
   const five = new Dollar(5);
-  five.times(2);
-  expect(five.amount).toBe(10);
+  const product = five.times(2);
+  expect(product.amount).toBe(10);
 });
 
 test("test multiplication, but value of Dollar should not be changed.", () => {
   const five = new Dollar(5);
-  five.times(2);
-  expect(five.amount).toBe(10);
-  five.times(3);
-  expect(five.amount).toBe(15);
+  let product = five.times(2);
+  expect(product.amount).toBe(10);
+  product = five.times(3);
+  expect(product.amount).toBe(15);
+});
+
+test("test equality", () => {
+  expect(new Dollar(5).equals(new Dollar(5))).toBeTruthy();
+  expect(new Dollar(5).equals(new Dollar(6))).toBeFalsy();
 });
